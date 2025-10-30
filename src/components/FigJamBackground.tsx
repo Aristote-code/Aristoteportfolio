c44224d88ef1858e6319175f4e969d14313d8e26
Aristote-code <Aristote-code@users.noreply.github.com>
2025-10-30 20:11:35 +0200

    Updated

--- src/components/FigJamBackground.tsx
+++ src/components/FigJamBackground.tsx
@@ -1,6 +1,6 @@
 export function FigJamBackground() {
   return (
-    <div className="absolute inset-0 -z-10 w-full min-h-full pointer-events-none">
+    <div className="bg-pattern">
       <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice">
         <defs>
           <pattern id="dotPattern" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
